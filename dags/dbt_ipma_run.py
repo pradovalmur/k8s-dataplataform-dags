65fd9b843fe7ebf0e468f4ec2156a4673aa21ac2
Valmur Prado <valmur.prado.external@sosafe.de>
2026-01-16 17:33:21 +0000

    new version

--- dags/dbt_ipma_run.py
+++ dags/dbt_ipma_run.py
@@ -13,36 +13,32 @@ with DAG(
 ) as dag:
 
     dbt_debug = BashOperator(
-    task_id="dbt_debug",
-    bash_command=f"""
-    set -euo pipefail
+        task_id="dbt_debug",
+        bash_command=f"""
+        set -euo pipefail
 
-    DBT_SRC=/opt/airflow/dags/repo/dags/dbt/ipma
-    DBT_RUN=/tmp/dbt_ipma
+        rm -rf {DBT_RUN}
+        mkdir -p {DBT_RUN}
 
-    rm -rf "$DBT_RUN"
-    mkdir -p "$DBT_RUN"
+        cp {DBT_SRC}/dbt_project.yml {DBT_RUN}/
+        cp {DBT_SRC}/profiles.yml {DBT_RUN}/
+        cp -R {DBT_SRC}/models {DBT_RUN}/
 
-    # copia só o projeto (evita carregar qualquer resíduo)
-    cp "$DBT_SRC/dbt_project.yml" "$DBT_RUN/"
-    cp "$DBT_SRC/profiles.yml" "$DBT_RUN/"
-    cp -R "$DBT_SRC/models" "$DBT_RUN/"
+        echo "---- dbt_project.yml ----"
+        cat {DBT_RUN}/dbt_project.yml
+        echo "---- procurando flags no project ----"
+        grep -n "flags" {DBT_RUN}/dbt_project.yml || true
 
-    echo "DBT_RUN content:"
-    find "$DBT_RUN" -maxdepth 2 -type f -print
-
-    dbt debug --profiles-dir /tmp/dbt_ipma --project-dir /tmp/dbt_ipma
-    """,
+        dbt debug --profiles-dir {DBT_RUN} --project-dir {DBT_RUN}
+        """,
     )
 
     dbt_run = BashOperator(
         task_id="dbt_run",
         bash_command=f"""
         set -euo pipefail
-        DBT_RUN=/tmp/dbt_ipma
-        dbt run --profiles-dir /tmp/dbt_ipma --project-dir /tmp/dbt_ipma
+        dbt run --profiles-dir {DBT_RUN} --project-dir {DBT_RUN}
         """,
     )
-
 
     dbt_debug >> dbt_run
