80dca0c30df83b4d4e215876a47fb1707bdf74bd
Valmur Prado <valmur.prado.external@sosafe.de>
2026-01-22 09:37:38 +0000

    update model

--- dags/dbt/ipma/models/curated/stg_ipma_observations.sql
+++ dags/dbt/ipma/models/curated/stg_ipma_observations.sql
@@ -1,61 +1,86 @@
-{{ config(materialized='table') }}
-
 with src as (
+
     select
         ingested_at,
         dt,
         hour,
-        observations
-    from {{ source('raw', 'ipma_observations_raw') }}
+        payload
+    from {{ source('ipma_raw', 'ipma_observations_raw') }}
+
 ),
 
--- observations é VARCHAR/JSON: {"2026-01-19T00:00": {"1210881": {...}, "1210883": {...}}}
-parsed as (
+-- transforma o payload (varchar JSON) num map(varchar, json)
+t1 as (
+
     select
         ingested_at,
         dt,
         hour,
-        cast(json_parse(observations) as map(varchar, json)) as by_ts
+        cast(json_parse(payload) as map(varchar, json)) as ts_map
     from src
+
 ),
 
-by_timestamp as (
+-- explode timestamps (YYYY-mm-ddThh:mm)
+t2 as (
+
     select
         ingested_at,
         dt,
         hour,
-        ts_key,
-        cast(ts_val as map(varchar, json)) as by_station
-    from parsed
-    cross join unnest(map_entries(by_ts)) as t(ts_key, ts_val)
+        ts_key as observed_at_str,
+        ts_val as stations_json
+    from t1
+    cross join unnest(map_entries(ts_map)) as u(ts_key, ts_val)
+
 ),
 
-by_station as (
+-- stations_json é um objeto { "1210881": {...}, "1210883": null, ... }
+t3 as (
+
     select
         ingested_at,
         dt,
         hour,
-        ts_key,
-        station_id,
-        station_json
-    from by_timestamp
-    cross join unnest(map_entries(by_station)) as s(station_id, station_json)
+        from_iso8601_timestamp(observed_at_str) as observed_at,
+        cast(stations_json as map(varchar, json)) as station_map
+    from t2
+
+),
+
+-- explode stations
+t4 as (
+
+    select
+        ingested_at,
+        dt,
+        hour,
+        observed_at,
+        cast(station_id as integer) as idEstacao,
+        station_payload
+    from t3
+    cross join unnest(map_entries(station_map)) as s(station_id, station_payload)
+    where station_payload is not null
+
 )
 
 select
     ingested_at,
     dt,
     hour,
-    ts_key as observation_ts,
-    cast(station_id as varchar) as station_id,
+    observed_at,
 
-    try_cast(json_extract_scalar(station_json, '$.temperatura') as double)            as temperatura,
-    try_cast(json_extract_scalar(station_json, '$.humidade') as double)               as humidade,
-    try_cast(json_extract_scalar(station_json, '$.pressao') as double)                as pressao,
-    try_cast(json_extract_scalar(station_json, '$.radiacao') as double)               as radiacao,
-    try_cast(json_extract_scalar(station_json, '$.intensidadeVento') as double)       as intensidade_vento,
-    try_cast(json_extract_scalar(station_json, '$.intensidadeVentoKM') as double)     as intensidade_vento_km,
-    try_cast(json_extract_scalar(station_json, '$.idDireccVento') as integer)         as id_direcc_vento,
-    try_cast(json_extract_scalar(station_json, '$.precAcumulada') as double)          as prec_acumulada
+    idEstacao,
 
-from by_station
+    try_cast(json_extract_scalar(station_payload, '$.intensidadeVentoKM') as double) as intensidadeVentoKM,
+    try_cast(json_extract_scalar(station_payload, '$.temperatura') as double)          as temperatura,
+    try_cast(json_extract_scalar(station_payload, '$.radiacao') as double)             as radiacao,
+
+    try_cast(json_extract_scalar(station_payload, '$.idDireccVento') as integer)       as idDireccVento,
+    try_cast(json_extract_scalar(station_payload, '$.precAcumulada') as double)        as precAcumulada,
+    try_cast(json_extract_scalar(station_payload, '$.intensidadeVento') as double)     as intensidadeVento,
+    try_cast(json_extract_scalar(station_payload, '$.humidade') as double)             as humidade,
+    try_cast(json_extract_scalar(station_payload, '$.pressao') as double)              as pressao
+
+from t4
+;
